5a2ebc2c9b2b6d3d63c9a4202f842da858c9138a
fulutas <ulutasfurkan@gmail.com>
2020-11-02 10:48:06 +0300

    text to speech added. css fixes

---
+++ js/app.js
@@ -0,0 +1,36 @@
+const jokeApi = 'https://sv443.net/jokeapi/v2/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist';
+const jokeEl = document.getElementById('joke');
+
+function getJoke() {
+    fetch(jokeApi)
+        .then(response => response.json())
+        .then((data) => {
+            if (data.type == 'single') {
+                document.getElementById('joke').innerHTML = '"' + data.joke + '"'
+                document.getElementById('category').innerHTML = '' + data.category + ''
+
+            }
+            else {
+                document.getElementById('joke').innerHTML = '"' + data.setup + '</br></br>' + data.delivery + '"'
+                document.getElementById('category').innerHTML = '' + data.category + ''
+            }
+        })
+
+}
+
+function saveAsImage() {
+    const findEl = document.getElementById('frame1')
+    html2canvas(findEl).then((canvas) => {
+        const link = document.createElement('a')
+        document.body.appendChild(link)
+        link.download = "joke-image.jpg"
+        link.href = canvas.toDataURL()
+        link.click()
+        link.remove()
+    })
+}
+
+function textSpeak() {
+    let text = document.getElementById('joke').innerHTML;
+    responsiveVoice.speak(text);
+}
